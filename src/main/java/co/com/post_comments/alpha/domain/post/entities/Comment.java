e2c67c738847972dda7bedce637909ce89ac820a
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-29 16:06:58 -0500

    Define Comment entity on Post aggregate

---
+++ src/main/java/co/com/post_comments/alpha/domain/post/entities/Comment.java
@@ -0,0 +1,39 @@
+package co.com.post_comments.alpha.domain.post.entities;
+
+import co.com.post_comments.alpha.domain.post.identities.CommentId;
+import co.com.post_comments.alpha.domain.post.values.Author;
+import co.com.post_comments.alpha.domain.post.values.Content;
+import co.com.post_comments.alpha.domain.post.values.Date;
+import co.com.sofka.domain.generic.Entity;
+
+public class Comment extends Entity<CommentId> {
+    private final Author author;
+    private final Content content;
+    private final Date createdAt;
+
+    public Comment(Author author, Content content, Date createdAt) {
+        super(new CommentId());
+        this.author = author;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public Comment(CommentId entityId, Author author, Content content, Date createdAt) {
+        super(entityId);
+        this.author = author;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public Author author() {
+        return author;
+    }
+
+    public Content content() {
+        return content;
+    }
+
+    public Date createdAt() {
+        return createdAt;
+    }
+}
