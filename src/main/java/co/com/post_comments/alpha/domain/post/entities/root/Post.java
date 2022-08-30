acdcab0dc6778930b75101e87d7ee3561b3bae26
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-30 13:47:33 -0500

    Refactor Post aggregate
    Update behavious' input params

--- src/main/java/co/com/post_comments/alpha/domain/post/entities/root/Post.java
+++ src/main/java/co/com/post_comments/alpha/domain/post/entities/root/Post.java
@@ -3,6 +3,10 @@ package co.com.post_comments.alpha.domain.post.entities.root;
 import co.com.post_comments.alpha.domain.post.entities.Comment;
 import co.com.post_comments.alpha.domain.post.events.CommentAdded;
 import co.com.post_comments.alpha.domain.post.events.PostCreated;
+import co.com.post_comments.alpha.domain.post.values.Author;
+import co.com.post_comments.alpha.domain.post.values.Content;
+import co.com.post_comments.alpha.domain.post.values.Date;
+import co.com.post_comments.alpha.domain.post.values.Title;
 import co.com.post_comments.alpha.domain.post.values.identities.CommentId;
 import co.com.post_comments.alpha.domain.post.values.identities.PostId;
 import co.com.sofka.domain.generic.AggregateEvent;
@@ -25,10 +29,10 @@ public class Post extends AggregateEvent<PostId> {
         super.subscribe(new PostEventListener(this));
     }
 
-    public Post(PostId entityId, String author, String title, String postedAt) {
+    public Post(PostId entityId, Author author, Title title, Date postedAt) {
         super(entityId);
         super
-                .appendChange(new PostCreated(entityId.value(), title, author, postedAt))
+                .appendChange(new PostCreated(entityId.value(), title.value(), author.value(), postedAt.value().toString()))
                 .apply();
     }
 
@@ -38,9 +42,9 @@ public class Post extends AggregateEvent<PostId> {
         return post;
     }
 
-    public void addComment(PostId postId, CommentId commentId, String commentAuthor, String commentContent, String postedAt) {
+    public void addComment(PostId postId, CommentId commentId, Author commentAuthor, Content commentContent, Date postedAt) {
         super
-                .appendChange(new CommentAdded(postId.value(), commentId.value(), commentAuthor, commentContent, postedAt))
+                .appendChange(new CommentAdded(postId.value(), commentId.value(), commentAuthor.value(), commentContent.value(), postedAt.value().toString()))
                 .apply();
     }
 
