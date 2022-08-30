16567cd61c417ba732ad5868c79cc4b60d7f58c0
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-30 12:07:41 -0500

    Make Post.events.CommentAdded's fields not final

--- src/main/java/co/com/post_comments/alpha/domain/post/events/CommentAdded.java
+++ src/main/java/co/com/post_comments/alpha/domain/post/events/CommentAdded.java
@@ -1,22 +1,23 @@
 package co.com.post_comments.alpha.domain.post.events;
 
-import co.com.post_comments.alpha.domain.post.values.identities.CommentId;
-import co.com.post_comments.alpha.domain.post.values.identities.PostId;
-import co.com.post_comments.alpha.domain.post.values.Author;
-import co.com.post_comments.alpha.domain.post.values.Content;
-import co.com.post_comments.alpha.domain.post.values.Date;
 import co.com.sofka.domain.generic.DomainEvent;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class CommentAdded extends DomainEvent {
-    private final PostId postId;
-    private final CommentId commentId;
-    private final Author author;
-    private final Content content;
-    private final Date postedAt;
+    private String postId;
+    private String commentId;
+    private String author;
+    private String content;
+    private String postedAt;
 
-    public CommentAdded(PostId postId, CommentId commentId, Author author, Content content, Date postedAt) {
+    public CommentAdded() {
+        super("PostAndComments.posts.AddedComment");
+    }
+
+    public CommentAdded(String postId, String commentId, String author, String content, String postedAt) {
         super("PostAndComments.posts.AddedComment");
         this.postId = postId;
         this.commentId = commentId;
@@ -25,23 +26,23 @@ public class CommentAdded extends DomainEvent {
         this.postedAt = postedAt;
     }
 
-    public PostId postId() {
+    public String postId() {
         return postId;
     }
 
-    public CommentId commentId() {
+    public String commentId() {
         return commentId;
     }
 
-    public Author author() {
+    public String author() {
         return author;
     }
 
-    public Content content() {
+    public String content() {
         return content;
     }
 
-    public Date postedAt() {
+    public String postedAt() {
         return postedAt;
     }
 }
