5e8c17e7bbb135c96a5b95611f275eabe4e4b78e
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-30 13:27:24 -0500

    Add Post.values.Date#parse

--- src/main/java/co/com/post_comments/alpha/domain/post/values/Date.java
+++ src/main/java/co/com/post_comments/alpha/domain/post/values/Date.java
@@ -9,6 +9,10 @@ import java.time.LocalDateTime;
 public class Date implements ValueObject<LocalDateTime> {
     private final LocalDateTime value;
 
+    public static Date parse(String value) {
+        return new Date(LocalDateTime.parse(value));
+    }
+
     @Override
     public LocalDateTime value() {
         return this.value;
