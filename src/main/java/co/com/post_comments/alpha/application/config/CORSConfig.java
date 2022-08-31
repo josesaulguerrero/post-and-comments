e412d8d895cfbd223d07b61d1a1ebe3aa80a1c56
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-31 18:32:20 -0500

    Set up CORS policy

---
+++ src/main/java/co/com/post_comments/alpha/application/config/CORSConfig.java
@@ -0,0 +1,21 @@
+package co.com.post_comments.alpha.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebFluxConfigurer CORSSetUp() {
+        return new WebFluxConfigurerComposite() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
