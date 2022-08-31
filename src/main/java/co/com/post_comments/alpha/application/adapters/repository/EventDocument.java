1b4824291b99cb78c8d3f50f9e3b6618febdd5f2
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-31 18:27:55 -0500

    Define the structure for the event document

---
+++ src/main/java/co/com/post_comments/alpha/application/adapters/repository/EventDocument.java
@@ -0,0 +1,32 @@
+package co.com.post_comments.alpha.application.adapters.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventDocument {
+
+    private String aggregateRootId;
+    private String jsonEvent;
+    private LocalDateTime timestamp;
+    private String eventType;
+
+    public String aggregateRootId() {
+        return aggregateRootId;
+    }
+
+    public String jsonEvent() {
+        return jsonEvent;
+    }
+
+    public LocalDateTime timestamp() {
+        return timestamp;
+    }
+
+    public String typeName() {
+        return eventType;
+    }
+}
