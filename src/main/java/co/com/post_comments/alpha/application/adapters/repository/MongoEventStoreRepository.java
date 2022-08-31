9e80cf425874a57e46a984a4a5a2b8f8c15eaa4b
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-31 18:58:07 -0500

    Define a mongo repository mapping

--- src/main/java/co/com/post_comments/alpha/application/adapters/repository/MongoEventStoreRepository.java
+++ src/main/java/co/com/post_comments/alpha/application/adapters/repository/MongoEventStoreRepository.java
@@ -1,9 +1,10 @@
-package com.posada.santiago.alphapostsandcomments.application.adapters.repository;
+package co.com.post_comments.alpha.application.adapters.repository;
 
+import co.com.post_comments.alpha.business.gateways.DomainEventRepository;
 import co.com.sofka.domain.generic.DomainEvent;
-import com.google.gson.Gson;
-import co.com.post_comments.alpha.application.adapters.repository.EventDocument;
-import com.posada.santiago.alphapostsandcomments.business.gateways.DomainEventRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -11,47 +12,53 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
 import java.util.Comparator;
-import java.util.Date;
 
 @Repository
+@AllArgsConstructor
 public class MongoEventStoreRepository implements DomainEventRepository {
 
-    private final ReactiveMongoTemplate template;
-
-    private final Gson gson = new Gson();
-
-    public MongoEventStoreRepository(ReactiveMongoTemplate template) {
-        this.template = template;
-    }
+    private final ReactiveMongoTemplate mongoTemplate;
+    private final ObjectMapper jsonMapper;
 
     @Override
-    public Flux<DomainEvent> findById(String aggregateId) {
+    public Flux<DomainEvent> findByAggregateId(String aggregateId) {
         var query = new Query(Criteria.where("aggregateRootId").is(aggregateId));
-        return template.find(query, DocumentEventStored.class)
-                .sort(Comparator.comparing(event -> event.getStoredEvent().getOccurredOn()))
-                .map(storeEvent -> {
+        return mongoTemplate.find(query, EventDocument.class, "alpha-events")
+                .sort(Comparator.comparing(EventDocument::timestamp))
+                .map(event -> {
                     try {
-                        return (DomainEvent) gson.fromJson(storeEvent.getStoredEvent().getEventBody(), Class.forName(storeEvent.getStoredEvent().getTypeName()));
-                    } catch (ClassNotFoundException e) {
+                        return (DomainEvent) jsonMapper.readValue(event.json(), Class.forName(event.eventType()));
+                    } catch (ClassNotFoundException | JsonProcessingException e) {
                         e.printStackTrace();
-                        throw new IllegalStateException("couldnt find domain event");
+                        throw new IllegalStateException("Couldn't cast the given JSON string to any Domain Event.");
                     }
                 });
     }
 
     @Override
-    public Mono<DomainEvent> saveEvent(DomainEvent event) {
-        DocumentEventStored eventStored = new DocumentEventStored();
-        eventStored.setAggregateRootId(event.aggregateRootId());
-        eventStored.setStoredEvent(new EventDocument(aggregateRootId, gson.toJson(event), new Date(), event.getClass().getCanonicalName()));
-        return template.save(eventStored)
-                .map(storeEvent -> {
+    public Mono<DomainEvent> save(DomainEvent event) {
+        return Mono.just(event)
+                .map(domainEvent -> {
                     try {
-                        return (DomainEvent) gson.fromJson(storeEvent.getStoredEvent().getEventBody(), Class.forName(storeEvent.getStoredEvent().getTypeName()));
-                    } catch (ClassNotFoundException e) {
+                        return new EventDocument(
+                                domainEvent.aggregateRootId(),
+                                this.jsonMapper.writeValueAsString(domainEvent),
+                                LocalDateTime.now(),
+                                domainEvent.getClass().getName()
+                        );
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .map(document -> {
+                    try {
+                        this.mongoTemplate.save(document, "alpha-events");
+                        return (DomainEvent) jsonMapper.readValue(document.json(), Class.forName(document.eventType()));
+                    } catch (ClassNotFoundException | JsonProcessingException e) {
                         e.printStackTrace();
-                        throw new IllegalStateException("couldnt find domain event");
+                        throw new IllegalStateException("Couldn't cast the given JSON string to any Domain Event.");
                     }
                 });
     }
