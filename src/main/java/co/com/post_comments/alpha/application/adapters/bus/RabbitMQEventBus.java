7fca1c6925ab905d26ece13fc6c782957cf181ac
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-01 09:17:42 -0500

    Refactor Notification
    Rename class from Notification to Message

--- src/main/java/co/com/post_comments/alpha/application/adapters/bus/RabbitMQEventBus.java
+++ src/main/java/co/com/post_comments/alpha/application/adapters/bus/RabbitMQEventBus.java
@@ -5,42 +5,40 @@ import co.com.post_comments.alpha.application.commons.JSONMapperImpl;
 import co.com.post_comments.alpha.application.config.RabbitMQConfig;
 import co.com.post_comments.alpha.business.gateways.EventBus;
 import co.com.sofka.domain.generic.DomainEvent;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RabbitMQEventBus implements EventBus {
     private static final JSONMapper jsonMapper = new JSONMapperImpl();
     private final RabbitTemplate rabbitTemplate;
 
-    public RabbitMQEventBus(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
-
     @Override
     public void publishDomainEvent(DomainEvent event) {
-        Notification notification = new Notification(
+        RabbitMQMessage rabbitMQMessage = new RabbitMQMessage(
                 event.getClass().getTypeName(),
                 jsonMapper.writeToJson(event)
         );
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.EXCHANGE,
                 RabbitMQConfig.MAIN_ROUTING_KEY,
-                notification.serialize().getBytes()
+                rabbitMQMessage.serialize().getBytes()
         );
     }
 
     @Override
     public void publishError(Throwable errorEvent) {
         ErrorEvent event = new ErrorEvent(errorEvent.getClass().getTypeName(), errorEvent.getMessage());
-        Notification notification = new Notification(
+        RabbitMQMessage rabbitMQMessage = new RabbitMQMessage(
                 event.getClass().getTypeName(),
                 jsonMapper.writeToJson(event)
         );
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.EXCHANGE,
                 RabbitMQConfig.MAIN_ROUTING_KEY,
-                notification.serialize().getBytes()
+                rabbitMQMessage.serialize().getBytes()
         );
     }
 }
