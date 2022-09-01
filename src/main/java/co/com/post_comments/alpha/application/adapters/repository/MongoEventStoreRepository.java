8a462c4718685e65ee7451ce3b60516c0acbbcde
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-31 21:10:39 -0500

    Fix the save event logic

--- src/main/java/co/com/post_comments/alpha/application/adapters/repository/MongoEventStoreRepository.java
+++ src/main/java/co/com/post_comments/alpha/application/adapters/repository/MongoEventStoreRepository.java
@@ -1,9 +1,8 @@
 package co.com.post_comments.alpha.application.adapters.repository;
 
+import co.com.post_comments.alpha.application.commons.JSONMapper;
 import co.com.post_comments.alpha.business.gateways.DomainEventRepository;
 import co.com.sofka.domain.generic.DomainEvent;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -20,19 +19,18 @@ import java.util.Comparator;
 public class MongoEventStoreRepository implements DomainEventRepository {
 
     private final ReactiveMongoTemplate mongoTemplate;
-    private final ObjectMapper jsonMapper;
+    private final JSONMapper jsonMapper;
 
     @Override
     public Flux<DomainEvent> findByAggregateId(String aggregateId) {
         var query = new Query(Criteria.where("aggregateRootId").is(aggregateId));
-        return mongoTemplate.find(query, EventDocument.class, "alpha-events")
+        return mongoTemplate.find(query, EventDocument.class, "events")
                 .sort(Comparator.comparing(EventDocument::timestamp))
                 .map(event -> {
                     try {
-                        return (DomainEvent) jsonMapper.readValue(event.json(), Class.forName(event.eventType()));
-                    } catch (ClassNotFoundException | JsonProcessingException e) {
-                        e.printStackTrace();
-                        throw new IllegalStateException("Couldn't cast the given JSON string to any Domain Event.");
+                        return (DomainEvent) jsonMapper.readFromJson(event.eventJSON(), Class.forName(event.eventType()));
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
                     }
                 });
     }
@@ -40,26 +38,24 @@ public class MongoEventStoreRepository implements DomainEventRepository {
     @Override
     public Mono<DomainEvent> save(DomainEvent event) {
         return Mono.just(event)
-                .map(domainEvent -> {
-                    try {
-                        return new EventDocument(
+                .map(domainEvent ->
+                        new EventDocument(
                                 domainEvent.aggregateRootId(),
-                                this.jsonMapper.writeValueAsString(domainEvent),
+                                this.jsonMapper.writeToJson(domainEvent),
                                 LocalDateTime.now(),
                                 domainEvent.getClass().getName()
-                        );
-                    } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .map(document -> {
-                    try {
-                        this.mongoTemplate.save(document, "alpha-events");
-                        return (DomainEvent) jsonMapper.readValue(document.json(), Class.forName(document.eventType()));
-                    } catch (ClassNotFoundException | JsonProcessingException e) {
-                        e.printStackTrace();
-                        throw new IllegalStateException("Couldn't cast the given JSON string to any Domain Event.");
-                    }
-                });
+                        )
+                )
+                .flatMap(document ->
+                        this.mongoTemplate.save(document, "events")
+                                .map(documentFromDB -> {
+                                    try {
+                                        return (DomainEvent) this.jsonMapper.readFromJson(documentFromDB.eventJSON(), Class.forName(documentFromDB.eventType()));
+                                    } catch (ClassNotFoundException e) {
+                                        e.printStackTrace();
+                                        throw new IllegalStateException("Couldn't cast the given JSON string to any Domain Event.");
+                                    }
+                                })
+                );
     }
 }
