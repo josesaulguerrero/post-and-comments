9bb56140a652172b2f84fb02d7ee3ddf63d914de
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-31 19:24:04 -0500

    Make commands' fields not final and add no-args constructor

--- src/main/java/co/com/post_comments/alpha/domain/post/commands/AddComment.java
+++ src/main/java/co/com/post_comments/alpha/domain/post/commands/AddComment.java
@@ -3,16 +3,18 @@ package co.com.post_comments.alpha.domain.post.commands;
 import co.com.sofka.domain.generic.Command;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class AddComment extends Command {
-    private final String postId;
-    private final String author;
-    private final String content;
-    private final String postedAt;
+    private String postId;
+    private String author;
+    private String content;
+    private String postedAt;
 
     public String postId() {
         return postId;
--- src/main/java/co/com/post_comments/alpha/domain/post/commands/CreatePost.java
+++ src/main/java/co/com/post_comments/alpha/domain/post/commands/CreatePost.java
@@ -3,15 +3,17 @@ package co.com.post_comments.alpha.domain.post.commands;
 import co.com.sofka.domain.generic.Command;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class CreatePost extends Command {
-    private final String title;
-    private final String author;
-    private final String postedAt;
+    private String title;
+    private String author;
+    private String postedAt;
 
     public String title() {
         return title;
