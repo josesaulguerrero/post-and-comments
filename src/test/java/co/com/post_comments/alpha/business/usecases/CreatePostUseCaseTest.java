95134c516f74ff4ed6177c1902f19ca2ab39853b
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-01 14:04:46 -0500

    Define the success case for the CreatePost use case

---
+++ src/test/java/co/com/post_comments/alpha/business/usecases/CreatePostUseCaseTest.java
@@ -0,0 +1,70 @@
+package co.com.post_comments.alpha.business.usecases;
+
+import co.com.post_comments.alpha.business.gateways.DomainEventRepository;
+import co.com.post_comments.alpha.business.gateways.EventBus;
+import co.com.post_comments.alpha.domain.post.commands.CreatePost;
+import co.com.post_comments.alpha.domain.post.events.PostCreated;
+import co.com.sofka.domain.generic.DomainEvent;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CreatePostUseCaseTest {
+    @Mock
+    private EventBus eventBus;
+
+    @Mock
+    private DomainEventRepository eventRepository;
+
+    @InjectMocks
+    private CreatePostUseCase useCase;
+
+    @Test
+    @DisplayName("The use case should save a Post and trigger an event of type PostCreated when successful.")
+    void apply_ShouldCreateANewPost_WhenSuccessful() {
+        // Arrange
+        CreatePost command = new CreatePost(
+                "Test post",
+                "Tester",
+                LocalDateTime.now().toString()
+        );
+        PostCreated event = new PostCreated(
+                UUID.randomUUID().toString(),
+                command.title(),
+                command.author(),
+                command.postedAt()
+        );
+
+        BDDMockito
+                .when(this.eventRepository.save(ArgumentMatchers.any(DomainEvent.class)))
+                .thenReturn(Mono.just(event));
+
+        // Act
+        Mono<List<DomainEvent>> triggeredEvents = this.useCase.apply(Mono.just(command))
+                .collectList();
+
+        // Assert
+        StepVerifier.create(triggeredEvents)
+                .expectSubscription()
+                .expectNextMatches(events ->
+                        events.size() == 1 &&
+                        events.get(0) instanceof PostCreated &&
+                        ((PostCreated) events.get(0)).author().equals("Tester")
+                )
+                .verifyComplete();
+        BDDMockito.verify(this.eventBus, BDDMockito.atMostOnce()).publishDomainEvent(ArgumentMatchers.any(DomainEvent.class));
+        BDDMockito.verify(this.eventRepository, BDDMockito.atMostOnce()).save(ArgumentMatchers.any(DomainEvent.class));
+    }
+}
