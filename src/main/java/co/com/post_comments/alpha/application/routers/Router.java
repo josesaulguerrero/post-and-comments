665f04a227bd3c01cd0cf0315da6f8de7870bdc2
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-31 22:00:51 -0500

    Add router method to change a comment content

--- src/main/java/co/com/post_comments/alpha/application/routers/Router.java
+++ src/main/java/co/com/post_comments/alpha/application/routers/Router.java
@@ -2,8 +2,10 @@ package co.com.post_comments.alpha.application.routers;
 
 
 import co.com.post_comments.alpha.business.usecases.AddCommentUseCase;
+import co.com.post_comments.alpha.business.usecases.ChangeCommentContentUseCase;
 import co.com.post_comments.alpha.business.usecases.CreatePostUseCase;
 import co.com.post_comments.alpha.domain.post.commands.AddComment;
+import co.com.post_comments.alpha.domain.post.commands.ChangeCommentContent;
 import co.com.post_comments.alpha.domain.post.commands.CreatePost;
 import co.com.sofka.domain.generic.DomainEvent;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +15,7 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -39,7 +40,6 @@ public class Router {
 
     @Bean
     public RouterFunction<ServerResponse> addComment(AddCommentUseCase useCase) {
-
         return route(
                 POST("add/comment").and(accept(MediaType.APPLICATION_JSON)),
                 request -> ServerResponse
@@ -48,6 +48,22 @@ public class Router {
                         .body(
                                 BodyInserters.fromPublisher(
                                         useCase.apply(request.bodyToMono(AddComment.class)),
+                                        DomainEvent.class
+                                )
+                        )
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> changeCommentContent(ChangeCommentContentUseCase useCase) {
+        return route(
+                PATCH("patch/comment").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(
+                                BodyInserters.fromPublisher(
+                                        useCase.apply(request.bodyToMono(ChangeCommentContent.class)),
                                         DomainEvent.class
                                 )
                         )
