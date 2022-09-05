c681d8d04f0ff1c9b0c162b172c6092b0ec29f84
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-05 14:13:54 -0500

    Add ISO formatter on Date VO

--- src/main/java/co/com/post_comments/alpha/domain/post/values/Date.java
+++ src/main/java/co/com/post_comments/alpha/domain/post/values/Date.java
@@ -4,13 +4,14 @@ import co.com.sofka.domain.generic.ValueObject;
 import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @AllArgsConstructor
 public class Date implements ValueObject<LocalDateTime> {
     private final LocalDateTime value;
 
     public static Date parse(String value) {
-        return new Date(LocalDateTime.parse(value));
+        return new Date(LocalDateTime.parse(value, DateTimeFormatter.ISO_DATE_TIME));
     }
 
     @Override
