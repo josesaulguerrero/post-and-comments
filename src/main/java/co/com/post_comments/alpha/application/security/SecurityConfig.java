fabaa7ba4c966ca884d27c40f7438c5c01404c57
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-13 16:39:42 -0500

    Set up Spring security

---
+++ src/main/java/co/com/post_comments/alpha/application/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package co.com.post_comments.alpha.application.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain getWetFilterBean(
+            ServerHttpSecurity http,
+            JWTConverter converter,
+            JWTAuthenticationManager authenticationManager
+    ) {
+        AuthenticationWebFilter jwtFilter = new AuthenticationWebFilter(authenticationManager);
+        jwtFilter.setServerAuthenticationConverter(converter);
+
+        return http
+                .csrf().disable()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                    .addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHORIZATION)
+                    .formLogin().disable()
+                    .httpBasic().disable()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
