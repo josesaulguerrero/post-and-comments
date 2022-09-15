07d3ae8d50c05862f4217667639d21c871978e8a
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-15 09:31:56 -0500

    Refactor CORS config

--- src/main/java/co/com/post_comments/alpha/application/config/CORSConfig.java
+++ src/main/java/co/com/post_comments/alpha/application/config/CORSConfig.java
@@ -1,23 +1,23 @@
 package co.com.post_comments.alpha.application.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
-public class CORSConfig {
-    @Bean
-    public WebFluxConfigurer CORSSetUp() {
-        return new WebFluxConfigurerComposite() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+@EnableWebFlux
+public class CORSConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "https://post-comments-c72b8.firebaseapp.com/app",
+                        "https://post-comments-c72b8.web.app/app",
+                        "http://localhost:[*]"
+                )
+                .allowedMethods("POST")
+                .maxAge(3600);
     }
 }
