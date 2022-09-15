2e6b7b4ef00bc94b7e6c13a0dc92e6da8d19f28c
Jose Saúl Serrano <josesaulserrano15@gmail.com>
2022-09-15 18:38:02 -0500

    Fix cors implementation

--- src/main/java/co/com/post_comments/alpha/application/config/CORSConfig.java
+++ src/main/java/co/com/post_comments/alpha/application/config/CORSConfig.java
@@ -8,15 +8,15 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 @EnableWebFlux
 public class CORSConfig implements WebFluxConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .allowedOriginPatterns(
-                        "https://post-comments-c72b8.firebaseapp.com/app",
-                        "https://post-comments-c72b8.web.app/app",
+                        "https://post-comments-c72b8.firebaseapp.com",
+                        "https://post-comments-c72b8.web.app",
                         "http://localhost:[*]"
                 )
+                .allowedHeaders("*")
                 .allowedMethods("POST")
                 .maxAge(3600);
     }
