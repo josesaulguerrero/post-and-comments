893cd6be94fb9bff236aebbc96db76ef176dfa3d
Jose Saúl Serrano <josesaulserrano15@gmail.com>
2022-09-15 20:35:02 -0500

    Add logs on the event bus

--- src/main/java/co/com/post_comments/alpha/application/adapters/bus/RabbitMQEventBus.java
+++ src/main/java/co/com/post_comments/alpha/application/adapters/bus/RabbitMQEventBus.java
@@ -6,9 +6,11 @@ import co.com.post_comments.alpha.application.config.RabbitMQConfig;
 import co.com.post_comments.alpha.business.gateways.EventBus;
 import co.com.sofka.domain.generic.DomainEvent;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class RabbitMQEventBus implements EventBus {
@@ -17,6 +19,7 @@ public class RabbitMQEventBus implements EventBus {
 
     @Override
     public void publishDomainEvent(DomainEvent event) {
+        log.info("Publishing a domain event to RabbitMQ with the following information: " + event);
         RabbitMQMessage rabbitMQMessage = new RabbitMQMessage(
                 event.getClass().getTypeName(),
                 jsonMapper.writeToJson(event)
@@ -30,6 +33,7 @@ public class RabbitMQEventBus implements EventBus {
 
     @Override
     public void publishError(Throwable errorEvent) {
+        log.error("Publishing an error to RabbitMQ with the following information: " + errorEvent);
         RabbitMQErrorEvent event = new RabbitMQErrorEvent(errorEvent.getClass().getTypeName(), errorEvent.getMessage());
         RabbitMQMessage rabbitMQMessage = new RabbitMQMessage(
                 event.getClass().getTypeName(),
